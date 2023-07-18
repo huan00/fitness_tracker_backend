d6d0138598b79cfd05f11dc680e19105e90a4587
huan00 <96999144+huan00@users.noreply.github.com>
2023-07-18 11:57:20 -0400

    add all hosts

--- fitness_app/fitness_app/settings.py
+++ fitness_app/fitness_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-xgpkiqnpoy%01_g(6gsuiu($5mhn!-avwyw9842p7d$p4@7h=#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', '192.168.4.27', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
